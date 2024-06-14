8b5f9a3a51e6eaefda0bd781df4927f479e6bbd1
TheWarfarer <101476791+Thewarfarer@users.noreply.github.com>
2024-06-14 16:21:48 +0530

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1teDzX4GA5r4GQbU9KKBZffM_YUkcefnNbLo_zlXygnc/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyDpiINfPRxPrDsVJBYpLRpWZ9BjHfM894c';// 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyAqQ50aGLhUS1gKNpAzOQpnDcSWNDO7IUg';// 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
